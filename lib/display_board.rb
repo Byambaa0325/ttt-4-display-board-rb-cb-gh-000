4e24f030a182a52769922873a11d162706d0f9cf
Byambaa Bayarmandakh <byambaa0325@gmail.com>
2018-08-07 07:51:19 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board(data=)
+def display_board(data= [" "," "," "," "," "," "," "," "," "])
   puts(" #{data[0]} | #{data[1]} | #{data[2]} \n  ----------- \n  #{data[3]} | #{data[4]} | #{data[5]} \n ----------- \n  #{data[6]} | #{data[7]} | #{data[8]} ")
 end
 
