9f6b65f1d0f15e0c3abe97fb89824dc5326c4c2a
Byambaa Bayarmandakh <byambaa0325@gmail.com>
2018-08-07 07:48:14 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(data)
+  puts(" #{data[0]} | #{data[1]} | #{data[2]} 
+  #{data[3]} | #{data[4]} | #{data[5]} 
+ -----------
+  #{data[6]} | #{data[7]} | #{data[8]} ")
+end
